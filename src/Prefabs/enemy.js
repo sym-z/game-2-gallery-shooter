9631f7c798281936219937d709afecdd29548379
Jack Sims <123803257+sym-z@users.noreply.github.com>
2024-05-01 19:31:58 -0700

    horizontal movement, random cards
    need to lower card amount per row, and do multiple rows

--- src/Prefabs/enemy.js
+++ src/Prefabs/enemy.js
@@ -2,10 +2,16 @@ class Enemy extends Phaser.GameObjects.Sprite {
     constructor(scene, x,y, image) {
         super(scene , x, y, image);
         scene.add.existing(this)
+        this.direction = 1;
         
     }
     update() 
     {
-    
+        this.x += 3 * this.direction;
+    }
+
+    switch_direction()
+    {
+        this.direction *= -1;
     }
 }
--- src/Scenes/level.js
+++ src/Scenes/level.js
@@ -8,24 +8,71 @@ class Level extends Phaser.Scene
         this.playerY = 500
         this.playerShotX = this.playerX
         this.playerShotY = this.playerY - 10;
+        this.num_enemies = 3;
+
+        this.enemies = []
+        this.cards = 
+        {
+            
+
+        }
     }
 
     preload() 
     {
         this.load.setPath("./assets/");
         this.load.image("Joker", "large-cards/card_joker_black.png");
+        
+        for (let i = 0; i < 5; i++)
+        {
+            let title = this.generate_card()
+            this.load.image("Enemy" + i, title)
+            console.log("Enemy"+i)
+        }
     }
 
     create() 
     {
-        //this.x = 0.0
+        for(let i = 0; i < 5; i++)
+        {
+            this.enemy = new Enemy(this, 400+i*75,300,"Enemy"+i);
+            this.enemies.push(this.enemy);
+        }
+        console.log(this.enemies)
         this.me = new Player(this,this.playerX,this.playerY,"Joker").setOrigin(0.5);
     }
 
     update(time, delta) 
     {
         //this.x += delta
+        for (let e of this.enemies)
+        {
+            e.update();
+            if (e.x >= 800 || e.x <= 0)
+            {
+                this.flip()
+            }
+        }
+
         //console.log(this.x/1000)
     }
-
+    generate_card()
+    {
+        let card = "card_";
+        let deck = ["A", "02", "03", "04", "05", "06", "07", "08", "09", "10", "J","Q","K"]
+        let suits = ["diamonds","clubs","spades","hearts"]
+        let rand_deck = deck[Phaser.Math.Between(0,deck.length-1)]
+        let rand_suit = suits[Phaser.Math.Between(0,suits.length-1)]
+        card += rand_suit + "_" + rand_deck + ".png";
+        //this.load.setPath("./assets/large-cards/");
+        card = "large-cards/" + card;
+        return card;
+    }
+    flip()
+    {
+        for (let e of this.enemies)
+        {
+            e.switch_direction();
+        }
+    }
 }
